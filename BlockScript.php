c88ee28065047b7f7174ddd41709a7b61e136ff2
SoniaVarian <soniavalmai15@gmail.com>
2017-05-29 15:16:56 +0700

    Add files via upload

---
+++ BlockScript.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Contoh Pemakaian ScriptPHP</title>
+</head>
+<body>
+<? //register global on
+echo "Aku sekarang bisa PHP!<br>";
+print "Mudah dan menyenangkan<br>";
+?>
+<?php echo("ini juga bisa <br>"); ?>
+
+<script language="php">
+echo ("tulis pake ini jika html editor tidak mengenali PHP");
+</script>
+</body>
+</html>
+
